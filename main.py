c3345515539866da2523f831ee1be89982253846
Jowel123v <jdmarin12@ucatolica.edu.co>
2025-10-29 16:29:16 -0500

    Endpoints del modelo estudiante completos

--- main.py
+++ main.py
@@ -60,3 +60,47 @@ def root():
 def health():
     return {"status": "ok"}
 
+# ESTUDIANTES
+
+@app.post("/estudiantes/", response_model=Estudiante, tags=["Estudiantes"])
+def crear_nuevo_estudiante(obj: Estudiante, session: Session = Depends(get_session)):
+    return crear_estudiante(session, obj)
+
+@app.get("/estudiantes/", response_model=List[Estudiante], tags=["Estudiantes"])
+def listar_todos_los_estudiantes(
+    skip: int = 0,
+    limit: int = Query(10, le=100),
+    include_deleted: bool = Query(False, description="Incluir eliminados lógicamente"),
+    semestre: Optional[int] = Query(None, ge=1, le=10),
+    nombre: Optional[str] = None,
+    session: Session = Depends(get_session)
+):
+    return listar_estudiantes(session, skip, limit, include_deleted, semestre, nombre)
+
+@app.get("/estudiantes/deleted", response_model=List[Estudiante], tags=["Estudiantes"])
+def listar_estudiantes_borrados(session: Session = Depends(get_session)):
+    return listar_estudiantes_eliminados(session)
+
+@app.post("/estudiantes/{estudiante_id}/restore", tags=["Estudiantes"])
+def restaurar_estudiante_por_id(estudiante_id: int, session: Session = Depends(get_session)):
+    if restaurar_estudiante(session, estudiante_id):
+        return {"message": "Estudiante restaurado correctamente"}
+    raise HTTPException(status_code=404, detail="No fue posible restaurar el estudiante")
+
+@app.get("/estudiantes/search/", response_model=List[Estudiante], tags=["Estudiantes"])
+def buscar_estudiante(nombre: str = Query(..., min_length=1), session: Session = Depends(get_session)):
+    return buscar_estudiante_por_nombre(session, nombre)
+
+@app.get("/estudiantes/{estudiante_id}", response_model=Estudiante, tags=["Estudiantes"])
+def obtener_estudiante_por_id(estudiante_id: int, session: Session = Depends(get_session)):
+    return obtener_estudiante(session, estudiante_id)
+
+@app.put("/estudiantes/{estudiante_id}", response_model=Estudiante, tags=["Estudiantes"])
+def actualizar_datos_estudiante(estudiante_id: int, obj: Estudiante, session: Session = Depends(get_session)):
+    return actualizar_estudiante(session, estudiante_id, obj)
+
+@app.delete("/estudiantes/{estudiante_id}", tags=["Estudiantes"])
+def eliminar_estudiante_por_id(estudiante_id: int, session: Session = Depends(get_session)):
+    if eliminar_estudiante(session, estudiante_id):
+        return {"message": "Estudiante eliminado correctamente"}
+    raise HTTPException(status_code=404, detail="Estudiante no encontrado")
